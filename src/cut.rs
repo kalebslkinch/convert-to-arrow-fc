af41e79cdb5b872d05cc26537965058384c72e8b
Kaleb Kinch <63981060+kalebslkinch@users.noreply.github.com>
2023-08-27 22:50:00 +0000

    Include function name in default export

--- src/cut.rs
+++ src/cut.rs
@@ -1,13 +1,53 @@
 pub fn export_function(string: &String) -> String {
-    let first_string = &string.replace("export function", "const");
-    let mut second_string = String::new();
+    let mut new_string = String::new();
+  
+    let mut temp_string = String::new();
+
+    if string.contains("expor function") {
+        new_string.push_str(&string.replace("export function", "const"));
+    }
+
     if string.contains("({}) {") {
-        second_string.push_str(&first_string.replace("({}) {", ": FC = ({}) => {"));
+        temp_string.push_str(&new_string.replace("({}) {", ": FC = ({}) => {"));
+        new_string.clear();
+        new_string.push_str(&temp_string);
+        temp_string.clear();
     }
     if string.contains("() {") {
-        second_string.push_str(&first_string.replace("() {", " = () => {"));
+        temp_string.push_str(&new_string.replace("() {", ": FC = () => {"));
+        new_string.clear();
+        new_string.push_str(&temp_string);
+        temp_string.clear();
     }
-    let third_str = String::from("import type { FC } from react; \n") + &second_string;
-    let fourth_str = third_str + &String::from("\n export default ");
-    fourth_str
+
+
+    if string.contains("import React from 'react';") {
+        temp_string.push_str(&new_string.replace("import React from 'react';", "import React, { FC } from 'react';\n"));
+        new_string.clear();
+        new_string.push_str(&temp_string);
+        temp_string.clear();
+    }
+
+    if string.contains("import React from \"react\";") {
+        temp_string.push_str(&new_string.replace("import React from \"react\";", "import React, { FC } from \"react\";\n"));
+        new_string.clear();
+        new_string.push_str(&temp_string);
+        temp_string.clear();
+    }   
+
+    if new_string.contains("const"){
+        let mut new_string_vec2: Vec<&str> = new_string.split(":").collect();
+        let mut new_string_vec3: Vec<&str> = new_string_vec2[0].split(" ").collect();
+        let mut new_string_vec4: Vec<&str> = new_string_vec3[new_string_vec3.len()-1].split("(").collect();
+        new_string.push_str(&format!("\nexport default {};", new_string_vec4[0]));
+    }
+
+        
+    new_string
+    
+
 }
+
+
+
+
--- test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Testing({}) {
-  return <div><h1>Something</h1></div>;
-}
-
-
-// Copy path of following file and use in terminal
-  
